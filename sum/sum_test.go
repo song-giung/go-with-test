d1bc9d142ac7aa856e642765ff03c7f83e0894a1
song-giung <rldnddl87@gmail.com>
2022-11-05 15:29:15 +0900

    Arrays and slices v3

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -27,3 +27,27 @@ func TestSumAll(t *testing.T) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
+
+func TestSumAllTails(t *testing.T) {
+
+	// 반복적인 코드를 줄임과 동시에 DeepEqual의 type safe 문제까지 해결했다.
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+
+	}
+
+	t.Run("make the sum of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+}
